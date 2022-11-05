7d26589a3aba5451e940e5a5ba3154d629891d3c
smasnb12 <smasnb12@gmail.com>
2022-11-05 15:29:24 +0500

    Chapter 1

---
+++ Chapter 1/alert.js
@@ -0,0 +1,15 @@
+// alert comment pop will appear that provide a message. 
+alert("Welcome to our organization!");
+// getting user input by prompt
+// prompt("Hi!, How are you?");
+// Random numbers
+Math.random();
+console.log(Math.random());
+
+// This number will be a decimal between 0 and 1. If we want a number
+// between 0 and 100, we can multiply it by 100
+
+console.log(Math.random() *100);
+
+// Math.floor is use to round off the numbers
+console.log(Math.floor(Math.random() *100 ));
