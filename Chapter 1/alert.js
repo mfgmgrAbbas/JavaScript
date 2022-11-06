656b3af707542e6db217216245cf16295ce8ed8b
smasnb12 <smasnb12@gmail.com>
2022-11-06 11:27:03 +0500

    Chapter 1

--- Chapter 1/alert.js
+++ Chapter 1/alert.js
@@ -1,15 +1,28 @@
 // alert comment pop will appear that provide a message. 
-alert("Welcome to our organization!");
+// alert("Welcome to our organization!");
 // getting user input by prompt
 // prompt("Hi!, How are you?");
+
 // Random numbers
 Math.random();
 console.log(Math.random());
 
-// This number will be a decimal between 0 and 1. If we want a number
-// between 0 and 100, we can multiply it by 100
+/* Output
+0.035616024946935276
+*/
+
+/*This number will be a decimal between 0 and 1. If we want a number
+between 0 and 100, we can multiply it by 100 */
 
 console.log(Math.random() *100);
 
+/* Output
+45.345702039931865
+*/
+
 // Math.floor is use to round off the numbers
 console.log(Math.floor(Math.random() *100 ));
+
+/* Output
+42
+*/
