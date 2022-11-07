cdf849baa4f4ce834f275ed09216ecc5c41d8590
smasnb12 <smasnb12@gmail.com>
2022-11-07 23:42:16 +0500

    for Loop array

---
+++ Loops/forLoopExmp.js
@@ -0,0 +1,26 @@
+const myWork = [];
+// for (let i = 1; i < 11; i++){
+//     let status = i % 2 ? true:false;
+//     let temporary = {
+//         name:`Lesson ${i}`,
+//         status: status
+//     }
+//     myWork.push(temporary);
+
+// }
+
+// console.log(myWork);
+/* Output
+[
+  { name: 'Lesson 1', status: true }, 
+  { name: 'Lesson 2', status: false },
+  { name: 'Lesson 3', status: true }, 
+  { name: 'Lesson 4', status: false },
+  { name: 'Lesson 5', status: true }, 
+  { name: 'Lesson 6', status: false },
+  { name: 'Lesson 7', status: true }, 
+  { name: 'Lesson 8', status: false },
+  { name: 'Lesson 9', status: true }, 
+  { name: 'Lesson 10', status: false }
+]
+*/
