c3bebfd815fafe70dd0c5147a7c6099e59a87bc8
smasnb12 <smasnb12@gmail.com>
2022-11-07 23:45:37 +0500

    AM operator, increment, decrement, concatnate

---
+++ Variables/operators.js
@@ -0,0 +1,56 @@
+let number = 20;
+let number1 = 35;
+console.log(number + number1);
+/* Output
+55
+*/
+let number3 = 45;
+let number4 = 50;
+console.log(number3 - number4);
+/* Output
+-5
+*/
+let number5 = 35;
+let number6 = 25;
+console.log(number5 * number6);
+/* Output
+875
+*/
+let number7 = 15;
+let number8 = 10;
+console.log(number7 / number8);
+/* Output
+1.5
+*/
+let number9 = 88;
+let number10 = 10;
+console.log(number9 % number10); // It gives reminder value
+/* Output
+8
+*/
+let number11 = 2;
+let number12 = 3;
+console.log(number11 ** number12); // It gives exponent
+/* Output
+8
+*/
+let number13 = 15;
+console.log(number13++); // 15
+console.log(++number13); // 17
+console.log(number13--); // 17
+console.log(--number13); // 15
+
+ let n = 25;
+ n += 5
+ console.log(n); //30
+ n -= 5
+ console.log(n); //20
+
+let firstName = "Muhammad";
+let middleName = "Abbas";
+let lastName = "Shah";
+
+console.log("Hello my name is " + firstName + ' ' + middleName + ' ' + lastName + '.');
+/* Output
+Hello my name is Muhammad Abbas Shah.
+*/
