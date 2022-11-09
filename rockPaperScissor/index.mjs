ccfc272a617cc52899eab2cd2e80518f604abd54
smasnb12 <smasnb12@gmail.com>
2022-11-09 18:20:34 +0500

    rockPaperScissor

---
+++ rockPaperScissor/index.mjs
@@ -0,0 +1,43 @@
+import promptSync from "prompt-sync";
+import randomInteger from "random-int";
+import randomInterger from "random-int";
+
+// The availble choices
+
+let choices = ["rock", "paper", "scissor"];
+
+// There will be the message that are shown to the user
+let userPrompt = "Please enter 0 for rock, 1 for paper, 2 for scissor";
+let outcome = "ready to play!";
+
+// Computer chose randomly
+let computerChoice = randomInteger(0, 2);
+let computerChoiceName = choices[computerChoice];
+// prompt the user for input
+
+const prompt = promptSync();
+const userInput = prompt(userPrompt);
+let userChoice = choices[userInput];
+if (computerChoiceName === userChoice){
+    outcome = "draw";
+}else if (computerChoiceName === "rock" && userChoice === "paper" ){
+    outcome = "Player Wins";
+}else if (computerChoiceName === "rock" && userChoice === "scissor"){
+    outcome = "Computer Wins"
+}else if (computerChoiceName === "paper" && userChoice === "rock"){
+    outcome = "Computer Wins";
+}else if (computerChoiceName === "paper" && userChoice === "scissor"){
+    outcome = "Palyer Winds";
+}else if (computerChoiceName === "scissor" && userChoice === "rock"){
+    outcome = "Palyer Winds";
+}else if (computerChoiceName === "scissor" && userChoice === "paper"){
+    outcome = "Computer Wins";
+}else {
+    outcome = "Invalid entry. Please try again.";
+}
+console.log("Computer Chooses " + computerChoiceName);
+console.log("User Chooses " + userChoice);
+console.log(outcome);
+
+
+
