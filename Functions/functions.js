be9a69fb0a8f13bcdef13d35b311d61b99f46355
smasnb12 <smasnb12@gmail.com>
2022-11-11 20:49:51 +0500

    Functions

---
+++ Functions/functions.js
@@ -0,0 +1,35 @@
+function name(params) {
+    
+}
+
+// function for coffe Order
+function coffeOrder (){
+    console.log("Your coffe is on the way!");
+
+}
+coffeOrder();
+
+/* Your coffe is on the way! */
+
+function coffeOrder (){
+    return "Your coffe is on the way!";
+
+}
+const order1 = coffeOrder();
+console.log(order1);
+/* Your coffe is on the way! */
+
+function coffeOrder (drink){
+    return `Your coffe ${drink} is on the way!`;
+
+}
+const order2 = coffeOrder("Latte");
+console.log(order2);
+/* Your coffe Latte is on the way! */
+function coffeOrder (drink){
+    return `Your coffe ${drink} is on the way!`;
+
+}
+const order3 = coffeOrder("Esprsso");
+console.log(order3);
+/* Your coffe Esprsso is on the way! */
