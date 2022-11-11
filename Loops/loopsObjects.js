a79e2d8ded663ff24c0e0681ec3990d9bab4c791
smasnb12 <smasnb12@gmail.com>
2022-11-11 19:44:28 +0500

    LoopsObjects

---
+++ Loops/loopsObjects.js
@@ -0,0 +1,16 @@
+let car = {
+    make: "Land Rover",
+    model: "Defender 110",
+    year: "2024",
+    color: "Gray",
+}
+for (const key in car) {
+    console.log(key);
+}
+
+/* Output
+make
+model
+year
+color
+*/
