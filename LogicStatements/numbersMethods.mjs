e7f21bea3e571ad623131a611350f81d3100204e
smasnb12 <smasnb12@gmail.com>
2022-11-26 23:24:01 +0500

    Numbers Methods

---
+++ LogicStatements/numbersMethods.mjs
@@ -0,0 +1,16 @@
+console.log(Math);
+
+let pi = console.log(Math.PI);
+let piRound = Math.round(Math.PI);
+console.log(piRound);
+ let a = Math.ceil(2.100)
+ let b = Math.ceil(-2.1)
+ let c = Math.floor(2.10)
+ let e = Math.floor(-2.100)
+ console.log(`for a variable a: ${a} for a variable b: ${b} for variable c: ${c} for variable d: ${e}`);
+ 
+ let d = new Date();
+ let year = d.getFullYear();
+ console.log(year);
+ 
+ console.log(d); // 2022-11-26T16:53:03.979Z
